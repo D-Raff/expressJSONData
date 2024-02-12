4f5716866258ac40f978a089ddb3b08c4ced0abd
D-Raff <damonraffels@gmail.com>
2024-02-12 16:32:27 +0200

    'updates'

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('stuff');
+import express from 'express'
+
+const app = express()
+
+const router = express.Router()
+
+const port = +process.env.PORT || 4000
+//json url
+const dataURL = 'https://d-raff.github.io/VueJS-Portfolio-data/data/'
+// application middleware
+app.use(
+    router
+)
+
+router.get('^/$|/ejd', (req,res)=>{
+    res.json({
+        status: res.statusCode,
+        msg: "'You\'re home"
+    })
+})
+
+router.get('/education', async (req, res)=>{
+    let {education} = await (await fetch (dataURL)).json()
+    res.json({
+        status: res.statusCode,
+        education
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running at https://localhost:${port}`);
+})
